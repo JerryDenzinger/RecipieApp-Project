aea9c32bfafdc9baaea973d27752e6347e527409
JerryDenzinger <gerardo_denzinger@outlook.de>
2021-05-04 18:44:17 +0200

    Correction with error from hashcode and lombok

--- RecipeApp/src/main/java/com/jerry/www/RecipeApp/model/Category.java
+++ RecipeApp/src/main/java/com/jerry/www/RecipeApp/model/Category.java
@@ -9,7 +9,10 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Data
+@EqualsAndHashCode(exclude = { "recipes" })
 @Entity
 public class Category {
 	@Id
@@ -19,29 +22,5 @@ public class Category {
 
 	@ManyToMany(mappedBy = "categories")
 	private Set<Recipe> recipes;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getCategoryName() {
-		return categoryName;
-	}
-
-	public void setCategoryName(String categoryName) {
-		this.categoryName = categoryName;
-	}
-
-	public Set<Recipe> getRecipes() {
-		return recipes;
-	}
-
-	public void setRecipes(Set<Recipe> recipes) {
-		this.recipes = recipes;
-	}
 
 }
